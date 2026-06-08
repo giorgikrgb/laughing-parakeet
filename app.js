fd4b1e879fb5a474bcac0dedbd95354b98cf26a5
giorgikrgb <giorgikalabegashvili24@gmail.com>
2026-06-08 18:31:24 +0400

    thrd

--- app.js
+++ app.js
@@ -13,3 +13,63 @@ function updateClock() {
 updateClock();
 
 setInterval(updateClock, 1000);
+
+
+let currentSlide = 0;
+
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  showSlide(currentSlide);
+}
+
+let interval = setInterval(nextSlide, 3000);
+
+const slider = document.querySelector(".slider");
+
+slider.addEventListener("mouseenter", () => {
+  clearInterval(interval);
+});
+slider.addEventListener("mouseleave", () => {
+  interval = setInterval(nextSlide, 3000);
+});
+
+slider.addEventListener("mouseleave", () => {
+  interval = setInterval(nextSlide, 3000);
+});
+
+const slider = document.querySelector(".slider");
+
+slider.addEventListener("mouseenter", () => {
+  clearInterval(interval);
+});
+
+slider.addEventListener("mouseleave", () => {
+  interval = setInterval(nextSlide, 3000);
+});
+
+const buttonsContainer = document.getElementById("buttons");
+
+slides.forEach((slide, index) => {
+  const button = document.createElement("button");
+
+  button.textContent = index + 1;
+
+  button.addEventListener("click", () => {
+    currentSlide = index;
+    showSlide(currentSlide);
+  });
+
+  buttonsContainer.appendChild(button);
+});
